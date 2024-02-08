73daa428ca64d308aa36171c8612faff76c2a8a9
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-02-08 20:58:06 +0600

    some changes to the app users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+# from django.contrib.auth.decorators import login_required
 
 
 from .models import *
@@ -49,6 +52,40 @@ def user_login(request):
     return render(request, 'users/login.html', context=None)
 
 
+
+def profile(request, username):
+    user = User.objects.get(username = username)
+    return render(request, 'users/index.html', locals())
+
+
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    if request.user.username != username:
+        raise Http404("Нет доступа к данному профилю")
+    
+    if request.method == "POST":
+        new_username = request.POST.get('username')
+        new_first_name = request.POST.get('first_name')
+        new_last_name = request.POST.get('last_name')
+        new_location = request.POST.get('location')
+        new_job = request.POST.get('job')
+        new_email = request.POST.get('email')
+        try:
+            user.username = new_username
+            user.first_name = new_first_name
+            user.last_name = new_last_name
+            user.job = new_job
+            user.location = new_location
+            user.email = new_email
+            user.save()
+            return redirect('profile', user.username)
+        except Exception as e:
+            print(f"Error saving user: {e}")
+        return redirect('profile', user.username)
+
+    return render(request, 'users/edit.html', locals())
+
+
 def logout_view(request):
     logout(request)
     return redirect('index')
@@ -56,5 +93,3 @@ def logout_view(request):
 
 def reset(request):
     return render(request, 'users/reset.html', context=None)
-
-print("TEST")
