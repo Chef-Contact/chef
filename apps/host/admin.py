ee1ab40a19071d922ec45bff8c443ffcaefb5f76
Asylbek Turdaliev <turdalievasyl@gmail.com>
2024-02-14 01:54:33 +0600

    added modeltrans for base, host, filled up DB

--- apps/host/admin.py
+++ apps/host/admin.py
@@ -1,11 +1,67 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from apps.host import models 
 
+class BecomeaHostAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('image', 'image_end')}),
+        ('Russian Version', {'fields': (
+            'title_ru', 'descriptions_ru', 'title2_ru', 'title3_ru', 'title4_ru',
+            'descriptions2_ru', 'title5_ru', 'descriptions3_ru', 'title6_ru',
+            'descriptions4_ru', 'title_end_ru'
+        )}),
+        ('English Version', {'fields': (
+            'title_en', 'descriptions_en', 'title2_en', 'title3_en', 'title4_en',
+            'descriptions2_en', 'title5_en', 'descriptions3_en', 'title6_en',
+            'descriptions4_en', 'title_end_en'
+        )}),
+    )
+admin.site.register(models.BecomeaHost, BecomeaHostAdmin)
 
-admin.site.register(models.BecomeaHost)
-admin.site.register(models.Free)
-admin.site.register(models.Guests)
-admin.site.register(models.Receive)
-admin.site.register(models.BecomeEnd)
-admin.site.register(models.Blog)
-admin.site.register(models.BlogActive)
+class FreeAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('icon',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descritions_ru')}),
+        ('English Version', {'fields': ('title_en', 'descritions_en')}),
+    )
+admin.site.register(models.Free, FreeAdmin)
+
+class GuestsAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('icon',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descritions_ru')}),
+        ('English Version', {'fields': ('title_en', 'descritions_en')}),
+    )
+admin.site.register(models.Guests, GuestsAdmin)
+
+class ReceiveAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('icon',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descritions_ru')}),
+        ('English Version', {'fields': ('title_en', 'descritions_en')}),
+    )
+admin.site.register(models.Receive, ReceiveAdmin)
+
+class BecomeEndAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('icon',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descritions_ru')}),
+        ('English Version', {'fields': ('title_en', 'descritions_en')}),
+    )
+admin.site.register(models.BecomeEnd, BecomeEndAdmin)
+
+class BlogAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('image',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descriptions_ru',)}),
+        ('English Version', {'fields': ('title_en', 'descriptions_en',)}),
+    )
+admin.site.register(models.Blog, BlogAdmin)
+
+class BlogActiveAdmin(TranslationAdmin):
+    fieldsets = (
+        ('General', {'fields': ('image',)}),
+        ('Russian Version', {'fields': ('title_ru', 'descriptions_ru',)}),
+        ('English Version', {'fields': ('title_en', 'descriptions_en',)}),
+    )
+admin.site.register(models.BlogActive, BlogActiveAdmin)
