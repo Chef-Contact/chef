2b4cb6822be5b13cdcb61e02e60382facac8823f
bezbak <Cemetey.2019@gmail.com>
2024-02-19 03:42:37 +0600

    add createing chat, location in register shef and add cart

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -9,3 +9,19 @@ def chat_detail(request, id):
         Message.objects.create(text = text, user= request.user, chat = chat)
         return redirect('chat_detail', chat.id)
     return render(request, 'chats/chat.html', locals())
+
+def create_chat(request, user):
+    try:
+        print('try1')
+        try:
+            print('try1-1')
+            chat = Chat.objects.get(from_user = request.user, to_user = user)
+            return redirect('chat_detail', chat.id)
+        except:
+            print('except1-1')
+            chat = Chat.objects.get(to_user = request.user, from_user = user)
+            return redirect('chat_detail', chat.id)
+    except:
+        print('except2')
+        chat = Chat.objects.create(from_user = request.user, to_user = user)
+        return redirect('chat_detail', chat.id)
