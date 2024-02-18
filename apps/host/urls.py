9bbe593ed597d57812dfd58c1c25cec162746aec
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-02-19 02:29:50 +0600

    merged sima

--- apps/host/urls.py
+++ apps/host/urls.py
@@ -3,6 +3,7 @@ from apps.host import views
 
 urlpatterns = [
     path('becomeahost/', views.becomeahost, name='becomeahost'),
-    path('index_host/', views.index_host, name='index_host'),
-    path('shef_register/', views.shef_register, name='shef_register'),
+    path('index_event/', views.index_event, name='index_event'),
+    path('make_event/', views.make_event, name='make_event'),
+    path('event_detail/<int:id>/', views.event_detail, name="event_detail"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -9,6 +9,7 @@ from .models import *
 from apps.includes.models import HeaderTranslationModel, FooterTranslationModel
 from apps.base.models import Settings
 
+
 # Create your views here.
 def register(request):
     settings = Settings.objects.latest("id")
@@ -16,15 +17,19 @@ def register(request):
     footer = FooterTranslationModel.objects.latest('id')
 
     if request.method == "POST":
+        user_role = request.POST.get('user_role')
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
+        birthday = request.POST.get('birthday')
+        month_of_birth = request.POST.get('month_of_birth')
+        year_of_birth = request.POST.get('year_of_birth')
         print(username, email, password, confirm_password)
         if password == confirm_password:
             if username and email and password and confirm_password:
                 try:
-                    user = User.objects.create(username = username, email = email)
+                    user = User.objects.create(user_role = user_role, username = username, email = email, birthday = birthday, month_of_birth = month_of_birth, year_of_birth = year_of_birth)
                     user.set_password(password)
                     user.save()
                     user = User.objects.get(username = username)
@@ -41,7 +46,7 @@ def register(request):
             print("Пароли отличаются")
             return redirect('register')
 
-    return render(request, 'users/register.html', context=None)
+    return render(request, 'users/register.html', locals())
 
 def user_login(request):
     settings = Settings.objects.latest("id")
@@ -57,7 +62,7 @@ def user_login(request):
             return redirect('index')
         except:
             return redirect('login')
-    return render(request, 'users/login.html', context=None)
+    return render(request, 'users/login.html', locals())
 
 
 
@@ -70,6 +75,9 @@ def profile(request, username):
 
 
 def edit_profile(request, username):
+    settings = Settings.objects.latest("id")
+    header = HeaderTranslationModel.objects.latest("id")
+    footer = FooterTranslationModel.objects.latest('id')
     user = get_object_or_404(User, username=username)
     if request.user.username != username:
         raise Http404("Нет доступа к данному профилю")
@@ -83,6 +91,9 @@ def edit_profile(request, username):
         new_email = request.POST.get('email')
         new_biography = request.POST.get('biography')
         new_main_language = request.POST.get('main_language')
+        new_year_of_birth = request.POST.get('year_of_birth')
+        new_month_of_birth = request.POST.get('month_of_birth')
+        new_birthday = request.POST.get('birthday')
         try:
             user.username = new_username
             user.first_name = new_first_name
@@ -90,6 +101,9 @@ def edit_profile(request, username):
             user.job = new_job
             user.location = new_location
             user.email = new_email
+            user.birthday = new_birthday
+            user.month_of_birth = new_month_of_birth
+            user.year_of_birth = new_year_of_birth
             user.main_language = new_main_language
             user.biography = new_biography
             user.save()
@@ -100,6 +114,29 @@ def edit_profile(request, username):
 
     return render(request, 'users/edit.html', locals())
 
+def edit_profile_image(request, username):
+    settings = Settings.objects.latest("id")
+    header = HeaderTranslationModel.objects.latest("id")
+    footer = FooterTranslationModel.objects.latest('id')
+
+    if request.user.username != username:
+        raise Http404("Нет доступа к данному профилю")
+    
+    user = get_object_or_404(User, username=username)
+    return render(request, 'users/pic.html', locals())
+
+
+def verification(request, username):
+    settings = Settings.objects.latest("id")
+    header = HeaderTranslationModel.objects.latest("id")
+    footer = FooterTranslationModel.objects.latest('id')
+
+    if request.user.username != username:
+        raise Http404("Нет доступа к данному профилю")
+    
+    user = get_object_or_404(User, username=username)
+    return render(request, 'users/verification.html', locals())
+
 
 def logout_view(request):
     logout(request)
@@ -107,4 +144,8 @@ def logout_view(request):
 
 
 def reset(request):
-    return render(request, 'users/reset.html', context=None)
+    return render(request, 'users/reset.html', locals())
+
+
+def dishes(request):
+    return render(request, 'users/dishes.html', locals())
