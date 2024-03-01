17cc3e1fbbe26b98250996ada11e859824eb9302
bezbak <Cemetey.2019@gmail.com>
2024-03-02 02:13:56 +0600

    all_products

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.products.views import create_product
+from apps.products.views import create_product,all_products
 
 urlpatterns = [
     path('create_product/', create_product, name='create_product'),
+    path('all_products/<str:username>/', all_products, name='all_products'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from apps.products.models import ProductImages, Product,Kind,Category
+from apps.users.models import User
 from apps.host.models import Host
 from apps.includes.models import HeaderTranslationModel, FooterTranslationModel
-
+from apps.base.models import Settings
 
 # Create your views here.
 def create_product(request):
@@ -44,3 +45,12 @@ def create_product(request):
     kinds = Kind.objects.all()
     categories = Category.objects.all()
     return render(request, 'host/create_product.html', locals())
+
+
+def all_products(request,username):
+    user = User.objects.get(username = username)
+    settings = Settings.objects.latest("id")
+    header = HeaderTranslationModel.objects.latest("id")
+    footer = FooterTranslationModel.objects.latest('id')
+    user_shop = user.shop_user
+    return render(request, 'shop/all_products.html', locals())
