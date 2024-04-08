b891069d7ee45e45c36016d2b3b60647439b6b9e
admin <admin@ubuntu.localdomain>
2024-04-08 21:12:40 +0000

    isko

--- static/assets/js/shop_edit.js
+++ static/assets/js/shop_edit.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-    $('.image-btn').click(function() {
+$(document).ready(function () {
+    $('.image-btn').click(function () {
         var imageUrl = $(this).data('image-url');
         $('#modalImage').attr('src', imageUrl);
-        
     });
-    $('#selectDesignBtn').click(()=>{
+
+    const dd = document.querySelectorAll('.asdad')
+    for (let i = 0; i < dd.length; i++) {
+        dd[i].onclick = (e) => {
+            dd[i].style.border = '3px solid #f8c026'
+            for (let i = 0; i < dd.length; i++) {
+                if (dd[i] !== e.target.parentElement) {
+                    dd[i].style.border = 'none'
+                }
+            }
+
+            if (e.target.classList[0] == 'img-thumbnail') {
+                $('#imageIdInput').attr('value', e.target.parentElement.dataset.id);
+            } else {
+                $('#imageIdInput').attr('value', e.target.dataset.id);
+            }
+        }
+    }
+
+    $('#selectDesignBtn').click((e) => {
         var imageId = $('.image-btn').data('id');
-        $('#imageIdInput').attr('value', imageId);
 
     })
-    $('#selectDesignBtn').click(function() {
+    $('#selectDesignBtn').click(function () {
         $('#imageModal').modal('hide');
     });
 });
